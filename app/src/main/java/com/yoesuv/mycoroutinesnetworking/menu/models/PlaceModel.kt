2d361ce9af38d4c2c29ab40bce5e73b05ef21fa4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-09-18 14:43:44 +0700

    :sparkles: finishing app

--- app/src/main/java/com/yoesuv/mycoroutinesnetworking/menu/models/PlaceModel.kt
+++ app/src/main/java/com/yoesuv/mycoroutinesnetworking/menu/models/PlaceModel.kt
@@ -1,13 +1,17 @@
 package com.yoesuv.mycoroutinesnetworking.menu.models
 
 import android.os.Parcelable
+import androidx.annotation.Keep
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
+@Keep
 @Parcelize
 data class PlaceModel(
-    val name: String?,
-    val location: String?,
-    val description: String?,
-    val thumbnail: String?,
-    val image: String?
+    @SerializedName("nama") @Expose val name: String?,
+    @SerializedName("lokasi") @Expose val location: String?,
+    @SerializedName("deskripsi") @Expose val description: String?,
+    @SerializedName("thumbnail") @Expose val thumbnail: String?,
+    @SerializedName("gambar") @Expose val image: String?
 ) : Parcelable
--- app/src/main/java/com/yoesuv/mycoroutinesnetworking/utils/AppExtension.kt
+++ app/src/main/java/com/yoesuv/mycoroutinesnetworking/utils/AppExtension.kt
@@ -1,8 +1,12 @@
 package com.yoesuv.mycoroutinesnetworking.utils
 
 import android.util.Log
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import coil.api.load
 import com.yoesuv.mycoroutinesnetworking.BuildConfig
 import com.yoesuv.mycoroutinesnetworking.Constants
+import com.yoesuv.mycoroutinesnetworking.R
 
 fun logDebug(message: String) {
     if (BuildConfig.DEBUG) Log.d(Constants.TAG_DEBUG, message)
@@ -10,4 +14,13 @@ fun logDebug(message: String) {
 
 fun logError(message: String) {
     if (BuildConfig.DEBUG) Log.e(Constants.TAG_ERROR, message)
+}
+
+@BindingAdapter("loadImage")
+fun AppCompatImageView.loadImage(imageUrl: String?) {
+    this.load(imageUrl){
+        crossfade(true)
+        placeholder(R.drawable.placeholder_image)
+        error(R.drawable.placeholder_image)
+    }
 }
